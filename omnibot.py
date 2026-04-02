42fcb004fe92d8a0f44f8933f106452b78b5dbc1
JohanBCEkberg <johanbcekberg@gmail.com>
2026-04-02 13:26:54 +0200

    Read received data

--- omnibot.py
+++ omnibot.py
@@ -8,11 +8,24 @@ class Omnibot:
     self.connection = Connection(host, port, verbose=True)
     self.running: threading.Event = threading.Event()
     self.completed_path: threading.Event = threading.Event()
+    self.positions = []
+    self.velocities = []
+    self.accelerations = []
 
   def start(self):
     if self.running.is_set():
       print("Omnibot server is already running.")
       return
+
+    if len(self.positions) == 0 or len(self.velocities) == 0 or len(self.accelerations) == 0:
+      print("Reading trajectory data from traj.json...")
+      with open("traj.json", "r") as f:
+        import json
+        traj = json.load(f)
+        self.positions = traj.get("q", [])
+        self.velocities = traj.get("q_dot", [])
+        self.accelerations = traj.get("q_dot_dot", [])
+
     self.completed_path.clear()
     self.running.set()
 
