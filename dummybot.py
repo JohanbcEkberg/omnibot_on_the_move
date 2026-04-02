ad0c91bf8692b0c58476f235864e209fef557076
Ludvig Esbjörnsson <ludvig.esbjornsson@outlook.com>
2026-04-02 13:17:26 +0200

    Add files via upload

---
+++ dummybot.py
@@ -0,0 +1,52 @@
+# general
+import sys
+from time import time, sleep
+
+from omnibot.tcp import Connection
+
+
+def run_dummybot(HOST, PORT, verbose=True):
+    """
+    Runs a dummybot at host HOST that rotates a bit while printing x and y coordinates.
+    """
+    
+    ts = 0.1 # sampling time
+    tstart = time()
+
+    with Connection(HOST,PORT,verbose=verbose) as bot:
+        print("Connected")
+        
+        # Target speed for servos
+        vset = 100
+
+        # Go one way
+        while time() < tstart + 3:
+            t0 = time()
+
+            bot.set_speeds([vset,vset,vset])
+              
+            print('theta:'+str(bot.get_theta()))
+
+        # Go the other way
+        while time() < tstart + 6:
+            t0 = time()
+
+            bot.set_speeds([-vset,-vset,-vset])
+              
+            print('theta:'+str(bot.get_theta()))
+
+            sleep(max(0,t0+ts-time()))
+                    
+if __name__ == '__main__':
+    # Server settings
+    HOST = "localhost"
+    PORT = 9998
+
+    if len(sys.argv) > 2:
+        # If an input is given to the script, it will be interpreted as the intended
+        # IP-address and port. Baseline is localhost, 9998.
+        HOST = sys.argv[1]
+        PORT = sys.argv[2]
+    
+    print(f"HOST: \t {HOST}")
+    run_dummybot(HOST,PORT)
